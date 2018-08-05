54add1accc9e18bda1bc4db47fbda655079d97bc
Mauricio Ochoa <juanmauricio.ochoa@hotmail.com>
2018-08-05 16:10:08 -0500

    Add files via upload

---
+++ EjercicioGravedad/Assets/Scripts/ControlPersonaje.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlPersonaje : MonoBehaviour {
+
+    public float velocidad = 10f;
+    public float fuerzaSalto = 350f;
+    private bool enElSuelo = true;
+    private bool saltando = false;
+    private Vector3 direccion;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        rb.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        direccion = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")).normalized;
+
+        if (Input.GetKeyDown(KeyCode.Space) && saltando == false && enElSuelo == true)
+        {
+            saltando = true;
+            enElSuelo = false;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + transform.TransformDirection(direccion) * velocidad * Time.deltaTime);
+
+        if (saltando == true && enElSuelo == false)
+        {
+            rb.AddForce(transform.up * fuerzaSalto);
+            saltando = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision objeto)
+    {
+        if (objeto.gameObject.CompareTag("Planeta"))
+        {
+            enElSuelo = true;
+        }
+    }
+}
---
+++ EjercicioGravedad/Assets/Scripts/GravedadPlaneta.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravedadPlaneta : MonoBehaviour {
+
+    public float gravedad = 9.8f;
+    public float radioExterior = 12f;
+    public float radioInterior = 10f;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radioExterior);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radioInterior);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Collider[] objetos = Physics.OverlapSphere(transform.position, radioExterior);
+        foreach (Collider objeto in objetos)
+        {
+            if (Vector3.Distance(transform.position, objeto.transform.position) > radioInterior)
+            {
+                if (objeto.GetComponent<Rigidbody>())
+                {
+                    Rigidbody rb = objeto.GetComponent<Rigidbody>();
+                    rb.AddForce(gravedad * (transform.position - objeto.transform.position).normalized);
+
+                    Quaternion rotacionDestino = Quaternion.FromToRotation(objeto.transform.up, (objeto.transform.position - transform.position).normalized) * objeto.transform.rotation;
+                    objeto.transform.rotation = Quaternion.Slerp(objeto.transform.rotation, rotacionDestino, 10 * Time.deltaTime);
+                }
+            }
+        }
+	}
+}
